73403b90f6c8a82eb99147d6b08133ca1cad0837
hyerancho <129468803+hyerancho@users.noreply.github.com>
2025-04-05 15:13:30 +0900

    [Bronze II] Title: 수 정렬하기, Time: 112 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,34 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-public class Main {
-    public static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+ public class Main {
+        public static void main(String[] args) throws IOException{
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int N = Integer.parseInt(br.readLine());    
+            int[] numbers = new int[N]; 
+
+          
+            for(int i = 0; i < N; i++) {
+                numbers[i] = Integer.parseInt(br.readLine());
             }
+           
+            Arrays.sort(numbers);
+
+            StringBuilder sb = new StringBuilder();
+            for(int num : numbers) {
+                sb.append(num).append("\n");
+            }
+            System.out.print(sb);
         }
     }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] arr = new int[N];
-
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        bubbleSort(arr);
-
-        for (int num : arr) {
-            System.out.println(num);
-        }
-
-        sc.close();
-    }
-}
